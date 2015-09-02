315c3981a07b70f5736391196bee20b8bebfaab1
kjohann <kris-90@live.no>
2015-09-02 19:06:23 +0200

    Can create and fetch slideshow (not slides just yet)

--- client/editor/editor.js
+++ client/editor/editor.js
@@ -1,3 +1,14 @@
+var getFormData = function(selector) {
+    var data = $(selector).serializeArray();
+    return _.object(_.pluck(data, 'name'), _.pluck(data, 'value'));
+};
+
+var clearForm = function(formSelector) {
+   _.each($(formSelector), function(input) {
+       $(input).val('');
+   });    
+}
+
 Template.Editor.helpers({
 	editorOptions: function() {
          return {
@@ -10,4 +21,33 @@ Template.Editor.helpers({
         if(current) return current;
         return ' ';
     }
+});
+
+Template.Editor.events({
+   'click #slideshow-create': function(e) {
+       e.preventDefault();
+       var slideShow = getFormData('#slideshow-form');
+       
+       var existing = SlideShows.findOne({name: slideShow.name});
+       
+       if(typeof(existing) === 'undefined') {
+           SlideShows.insert(slideShow);
+           console.log('inserted new', slideShow);
+           clearForm('#slideshow-form input[type="text"]');
+       } else {
+           alert('A slideshow with that name already exists!'); //yep - that's user firendly and secure right there :)
+       }
+   },
+   'click #slideshow-fetch': function(e) {
+       e.preventDefault();
+       
+       var slideShowName = getFormData('#slideshow-form').name;
+       console.log(slideShowName);
+       var existing = SlideShows.findOne({name: slideShowName});
+       
+       if(typeof(existing) !== 'undefined') {
+           console.log('current slideshow set');
+           Session.set('currentShow', existing);
+       }
+   }
 });
